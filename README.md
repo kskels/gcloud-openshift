73a7beff82a2421cccbaa5f457dc6dd947b0b082
Kaspars Skels <kaspars.skels@gmail.com>
2021-12-07 16:51:02 -0600

    Add script for enabling google API services
    Signed-off-by: Kaspars Skels <kaspars.skels@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# gcloud-openshift
+# Installing an OCP4 cluster on GCP
+
+Scripts and tips for installing an OpenShift cluster on Google Cloud Platform.
---
+++ enable-services.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+GCP_API_SERVICES="compute.googleapis.com
+  cloudapis.googleapis.com
+  cloudresourcemanager.googleapis.com
+  dns.googleapis.com
+  iamcredentials.googleapis.com
+  iam.googleapis.com
+  servicemanagement.googleapis.com
+  serviceusage.googleapis.com
+  storage-api.googleapis.com
+  storage-component.googleapis.com
+  networksecurity.googleapis.com"
+
+for service in $GCP_API_SERVICES
+do
+  echo "Enabling API service: $service"
+  gcloud --verbosity="error" services enable $service
+done
